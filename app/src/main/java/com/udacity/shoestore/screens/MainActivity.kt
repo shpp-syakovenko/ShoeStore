36a9fd21aff50221e4a48c2d6f9035f8610fd5dc
Sergey Yakovenko <serglife777@gmail.com>
2021-09-01 16:57:38 +0300

    comment

--- app/src/main/java/com/udacity/shoestore/screens/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/screens/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var navController: NavController
+    // New
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
